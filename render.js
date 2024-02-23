6262200b0134dabaa65ca02924ab71ee89b3d802
Ev Bogue <ev@evbogue.com>
2024-02-22 18:31:24 -0600

    hide the raw data behind a button

--- render.js
+++ render.js
@@ -9,12 +9,22 @@ export const render = async (msg) => {
     timestamp.textContent = human(new Date(msg.timestamp))
   }, 10000)
 
+  const raw = h('code', {style: 'float: right;', onclick: () => {
+    const got = document.getElementById('raw:' + msg.hash)
+    if (!got) {
+      div.appendChild(h('pre', {id: 'raw:' + msg.hash}, [JSON.stringify(msg)]))
+    }
+    if (got) {
+      got.remove()
+    }
+  }}, ['raw '])
+
   const div = h('div', {classList: 'message', id: msg.hash}, [
     timestamp,
+    raw,
     await avatar(msg.author),
     h('br'),
     h('span', [msg.text]),
-    h('pre', [msg.raw])
   ])
   return div
 }
